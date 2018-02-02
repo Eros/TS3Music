976a0cf608af0e7479a4dc40bd4a4ed483947760
Eros <george.marr.cobc@gmail.com>
2018-02-02 19:50:01 +0000

    init

---
+++ cmake-build-debug/CMakeFiles/TS3Music.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TS3Music.dir/main.cpp.obj"
+  "TS3Music.pdb"
+  "TS3Music.exe"
+  "TS3Music.exe.manifest"
+  "libTS3Music.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TS3Music.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TS3Music.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TS3Music.
+# This may be replaced when dependencies are built.
